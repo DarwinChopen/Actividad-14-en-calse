989d151a41b19b11bd3c8fcb6d6e4d20fdc6f332
Darwin <jackdarwinbot@gmail.com>
2025-08-08 10:21:12 -0600

    segundo comit, se realizo el ingreso

--- actividad14encalse.py
+++ actividad14encalse.py
@@ -1,3 +1,14 @@
+def quick_sort(lista):
+    if len(lista) <= 1:
+        return lista
+
+    pivote = lista[0]
+    menores = [x for x in lista[1:] if x < pivote]
+    iguales = [x for x in lista if x == pivote]
+    mayores = [x for x in lista[1:] if x > pivote]
+
+    return quick_sort(menores) + iguales + quick_sort(mayores)
+participoantes={}
 while True:
     print("Menu")
     print("1.Ingreso")
@@ -11,7 +22,19 @@ while True:
     match opcion:
         case 1:
             print("Ingreso")
-
+            cantidad = int(input("¿Cuántos participantes desea ingresar? "))
+            for i in range(cantidad):
+                print(f"\nparticipante #{i + 1}")
+                while True:
+                   dorsal = int(input("Ingrese el número de dorsal: "))
+                   if dorsal in participoantes:
+                       print("Este dorsal ya existe Ingrese otro")
+                   else:
+                       break
+                participoantes[dorsal] = {}
+                participoantes[dorsal]["nombre"] = input("Ingrese el nombre: ")
+                participoantes[dorsal]["edad"] = int(input("Ingrese la edad: "))
+                participoantes[dorsal]["categoria"] = input("Ingrese la categoria: ")
         case 2:
             print("Mostrar por edad")
         case 3:
